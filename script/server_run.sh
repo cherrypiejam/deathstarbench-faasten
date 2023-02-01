81fe284617c6c77e8b3b2e57c1bad7d1dfd17917
Gongqi Huang <gongqih@hotmail.com>
2023-02-01 18:07:16 -0500

    fix minor

--- script/server_run.sh
+++ script/server_run.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-multivm --config config.yaml --listen_http 127.0.0.1:1337 --listen_sched 127.0.0.1:1338 --mem 500
+# multivm --config config.yaml --listen_http 127.0.0.1:1337 --listen_sched 127.0.0.1:1338 --mem 500
+
+PIDS=""
+scheduler --listen_http 127.0.0.1:1337 --listen_sched 127.0.0.1:1338 &
+PIDS="$PIDS $!"
+multivm --config config.yaml --listen_sched 127.0.0.1:1338 --mem 500 &
+PIDS="$PIDS $!"
+trap "kill -2 $PIDS" INT
+wait
